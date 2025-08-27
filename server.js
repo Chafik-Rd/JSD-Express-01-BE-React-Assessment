5f40dd3c5f0d1c80bc952f60adedffbf86f2bc19
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2025-08-27 19:09:49 +0700

    fix(server): edit cors option origin

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config()
 const app = express();
 
 const corsOption = {
-  origin: ["http://localhost:5174", "http://localhost:5175"],
+  origin: ["http://localhost:5173","http://localhost:5174", "http://localhost:5175"],
 };
 app.use(cors(corsOption));
 app.use(express.json());
